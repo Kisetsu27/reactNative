2b3204d5cf5df32b88faa67734b3c180ca8e49ac
Temuulen <kisetsu27@gmail.com>
2023-02-07 13:16:15 +0800

    a

--- minii-anhnii-app/App.js
+++ minii-anhnii-app/App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Hello World</Text>
-      <Image style={{width:50, height:50}} source={require("./assets/favicon.png")}></Image>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <ScrollView style={{height: 400}}>
+          <TouchableOpacity onPress={() => alert('zurag neg udaa daragdlaa')} onLongPress={()=> alert('zuragudaan daragdlaa')}>
+            <Image  style={{width: 500, height: 500}} source={require('./assets/tree-736885__480.jpg')}></Image>
+          </TouchableOpacity>
+        </ScrollView>
+        <ScrollView>
+          <Text>This is my first app</Text>
+        </ScrollView>
+        <View></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -17,8 +25,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  hello: {
-    fontSize: 36,
+    height:100
   }
 });
