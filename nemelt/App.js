de282cf7f5f4eb64425b98ea01458fbc12d8e3c9
Temuulen <kisetsu27@gmail.com>
2023-02-08 21:48:49 +0800

    a

--- nemelt/App.js
+++ nemelt/App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.counter}>0</Text>
+        <Text style={styles.label}>KM/H</Text>
+        <View style={styles.btnWrap}>
+          <Ionicons name="sunny" style={styles.btn}/>
+          <Ionicons name="code" style={styles.btn}/>
+        </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
   },
+  counter: {
+    fontSize: 200,
+    color: '#fff',
+  },
+  label: {
+    fontSize: 38,
+    color: '#fff',
+    fontWeight: '500'
+  },
+  btnWrap: {
+    position: 'absolute',
+    left: 15,
+    bottom: 15,
+    flexDirection: 'row',
+    // backgroundColor: 'red',
+    width: 100,
+    justifyContent: 'space-between'
+  },
+  btn: {
+    color: '#949494',
+    backgroundColor: '#1E1E1E',
+    fontSize: 32,
+    padding: 5,
+    borderRadius: 10,
+  }
 });
