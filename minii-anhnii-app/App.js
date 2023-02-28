722d4d32773c22a40e9373c58541673af325549b
Kisetsu27 <kisetsu27@gmail.com>
2023-02-28 13:00:13 +0800

    aa

--- minii-anhnii-app/App.js
+++ minii-anhnii-app/App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Text, View, Image, Button, ScrollView, TextInput,
+  SafeAreaView,
+  Platform,
+ } from "react-native";
+import { Home } from "./source/screens/Home.js"
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <ScrollView style={{height: 400}}>
-          <TouchableOpacity onPress={() => alert('zurag neg udaa daragdlaa')} onLongPress={()=> alert('zuragudaan daragdlaa')}>
-            <Image  style={{width: 500, height: 500}} source={require('./assets/tree-736885__480.jpg')}></Image>
-          </TouchableOpacity>
-        </ScrollView>
-        <ScrollView>
-          <Text>This is my first app</Text>
-        </ScrollView>
-        <View></View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+    </View>
   );
 }
 
