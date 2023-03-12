cb0c9c6b7b41e79168fb2fabf4303bd1979a0c49
Temuulen <kisetsu27@gmail.com>
2023-03-12 19:08:17 +0800

    mplus

---
+++ myApp/source/screens/Home.js
@@ -0,0 +1,171 @@
+import { View, Text, StyleSheet, StatusBar, SafeAreaView, Platform, Image, ScrollView} from 'react-native'
+import React from 'react'
+
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+
+import Ionicons  from '@expo/vector-icons/Ionicons'
+
+
+
+export default function Home() {
+  return (
+    <SafeAreaView style = {{ flex: 1}}>
+      <ScrollView  style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.headerLeft}>
+            <Text style = {styles.greeting}>Өдрийн мэнд</Text>
+          </View>
+          <View style = {styles.headerRight}>
+            <Image source = {require('../../assets/ticket.png')} style = {{width: 30}} />
+            <Image source = {require('../../assets/profile.png')} style = {styles.profile} />
+          </View>
+        </View>
+        <View style = {styles.cardContainer}>
+          <View style = {styles.card1}>
+            <Text style = {styles.cardText}>Товч ном</Text>
+            <Ionicons name ='triangle' style = {styles.cardShape}/>
+          </View>
+          <View style = {styles.card2}>
+            <Text style = {styles.cardText}>Цахим ном</Text>
+            <Ionicons name ='square' style = {styles.cardShape}/>
+          </View>
+          <View style = {styles.card3}>
+            <Text style = {styles.cardText}>Аудио ном</Text>
+            <Ionicons name ='play' style = {styles.cardShape}/>
+          </View>
+          <View style = {styles.card4}>
+            <Text style = {styles.cardText}>Подкаст</Text>
+            <Ionicons name = 'ellipse' style = {styles.cardShape} />
+          </View>
+        </View>
+        <View style = {styles.featureContainer}>
+          <View style = {styles.featureTop}>
+            <Text style = {styles.subTitle} >Бодит амьдралаас сэдэвлэв</Text>
+            <Text style = {styles.title} >Энэ 7 хоногийн онцлох</Text>
+          </View>
+          <Image source={require('../../assets/feat.jpg')} style = {{width: '100%' , height:500}} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F6F8',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    padding: 20,
+  },
+  scrollView: {
+    flex: 1,
+    padding: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  greeting: {
+    fontSize: 24,
+    fontWeight: 500
+  },
+  headerRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profile: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    marginLeft: 15,
+  },
+  cardContainer: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 20,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    gap: 13,
+    shadowColor: '#9e9e9e',
+    shadowOffset: {
+      width:0,
+      height: 0,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 20,
+  },
+  card1: {
+    backgroundColor: '#10C9CB',
+    width: '48%',
+    height: 100,
+    borderRadius: 20,
+    padding: 15
+  },
+  card2: {
+    backgroundColor: '#EE5557',
+    width: '48%',
+    height: 100,
+    borderRadius: 20,
+    padding: 15,
+  },
+  card3: {
+    backgroundColor: '#FFAE27',
+    width: '48%',
+    height: 100,
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'flex-start',
+  },
+  card4: {
+    backgroundColor: '#9662CE',
+    width: '48%',
+    height: 100,
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'flex-start',
+  },
+  cardText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 500
+  },
+  cardShape: {
+    color: '#fff',
+    fontSize: 32,
+  },
+  featureContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    gap: 13,
+    shadowColor: '#9e9e9e',
+    shadowOffset: {
+      width:0,
+      height: 0,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 20,
+    marginTop: 30,
+  },
+  featureTop: {
+    margin: 15
+  },
+  subTitle: {
+    fontWeight: 300,
+    textTransform: 'uppercase',
+    fontSize: 15,
+    color: 'gray'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize:32
+  }
+
+})
